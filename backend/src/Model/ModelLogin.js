d7e1edced10a5787adc450826dedc3d198aa17b9
kassio <kassioteadoro@gmail.com>
2024-02-02 16:54:07 -0300

    backend - criando primeira funçao model

---
+++ backend/src/Model/ModelLogin.js
@@ -0,0 +1,9 @@
+const connection = require('./connection')
+
+const register = async(nome, email, senha) => {
+    connection.execute(
+        `INSERT INTO database (nome, email, senha) VALUES
+            (?,?,?);`,
+            [nome,email,senha]
+    )
+}
