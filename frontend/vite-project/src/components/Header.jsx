4eda12743281b763b4553f9d7ecdc6ccb0a06492
KassioTeo <kassioteadoro@gmail.com>
2024-04-29 13:53:27 -0300

    brincando com pequenas funcoes

--- frontend/vite-project/src/components/Header.jsx
+++ frontend/vite-project/src/components/Header.jsx
@@ -3,11 +3,23 @@ import { useNavigate } from "react-router-dom";
 
 export default function Header() {
     const navigate = useNavigate()
-    return(
-        <header>
-        <h1>E-COMMERCE</h1>
-        <button onClick={ () => navigate('/register')}>ainda nao e cadastrado? Click aqui!!!</button>
-        <button onClick={ () => navigate('/login')}>ja tem cadastro? acesse a sua conta!!</button>
-        </header>
-    );
+
+    const client = JSON.parse(localStorage.getItem('client'));
+
+    if(client) {
+        return(
+            <header>
+                <h1>E-COMMERCE</h1>    
+                <p>seja bem vindo {client.nome}!!</p>
+            </header>
+            );
+    } else {
+        return(
+            <header>
+                <h1>E-COMMERCE</h1>
+                <button onClick={ () => navigate('/register')}>ainda nao e cadastrado? Click aqui!!!</button>
+                <button onClick={ () => navigate('/login')}>ja tem cadastro? acesse a sua conta!!</button>
+            </header>
+        );
+    }
 };
